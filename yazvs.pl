9812438b60a99687bac98289c510232c4b6e543d
Duane Wessels <dwessels@verisign.com>
2016-09-22 15:26:58 +0000

    update copyright year

--- yazvs.pl
+++ yazvs.pl
@@ -6,7 +6,7 @@
 # before they are published.
 #
 
-# Copyright (C) 2010 VeriSign, Inc
+# Copyright (C) 2016 VeriSign, Inc
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
--- zonediff.pl
+++ zonediff.pl
@@ -5,7 +5,7 @@
 # "diff" zone files after excluding certain RR types
 #
 
-# Copyright (C) 2010 VeriSign, Inc
+# Copyright (C) 2016 VeriSign, Inc
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
