verisign/yazvs

commit 3bde922fbd7b9e2df36d04b7368f7a105f540cc5

Fixed bug when handling revoked keys. Previously a revoked key was removed from the set of keys used for further validation of the zone.  The way this was done meant that the RRSIG covering DNSKEY could not validate.  Now the revoked key is kept but the sig_is_valid() function is changed to forbid use of a revoked key in validating non-DNSKEY RRsets.

yazvs.pl
@@ -170,8 +170,7 @@ sub candidate {
 	print "Crypto Validation of $zone_name_printable $candidate_serial\n";
 	print '-' x 70 ."\n";
 	ok("Parsed ". int(@$rrset). " RRs from $file");
-	@dnskeys = remove_revoked($rrset, @dnskeys);
-	@ksks = trusted_ksks(@dnskeys);
+	@ksks = trusted_ksks(remove_revoked($rrset, @dnskeys));
 	foreach my $rr (@$rrset) {
 		push(@nsset, $rr->nsdname) if 'NS' eq $rr->type && lc($zone_name) eq lc($rr->name);
 	}
@@ -187,7 +186,7 @@ sub candidate {
 		next unless $rr->type eq 'RRSIG';
 		next unless lc($rr->name) eq lc($zone_name);
 		next unless $rr->typecovered eq 'DNSKEY';
-		$x++ if Valid == sig_is_valid($rr, \@dnskeys, \@ksks);
+		$x++ if Valid == sig_is_valid($rr, \@dnskeys, \@dnskeys);
 	}
 	unless ($x) {
 		problem("Cannot validate DNSKEY RRset with KSKs");
@@ -445,6 +444,9 @@ sub sig_is_valid {
 	}
 	#print "Validating ". $rrsig->name. "/". $rrsig->typecovered. " RRSIG\n";
 	foreach my $key (@$dnskeys) {
+		if ($key->revoke) {
+			next unless 'DNSKEY' eq $rrsig->typecovered;
+		}
 		#print "   checking key ". $key->keytag."\n";
 		if ($rrsig->verify(\@data, $key)) {
 			debug("RRSIG/". $rrsig->keytag. " + DNSKEY/". $key->keytag. " signs ". $rrsig->name. "/". $rrsig->typecovered. " RRset");

commit da65a899852c12e81392613f66d37b29b23adfe6

Fix bug introduced with commit 3bde922f. The mistake was validating the apex DNSKEY RRset with itself, rather than the trust anchor(s).  For example, if the trust anchor validates one of the DNSKEYs, and if one (possibly different) DNSKEY signs the DNSKEY RRset, then the zone was mistakenly reported as valid.  Instead one of the TA-validated keys must sign the RRset.

yazvs.pl
@@ -196,7 +196,7 @@ sub candidate {
 		next unless $rr->type eq 'RRSIG';
 		next unless lc($rr->name) eq lc($zone_name);
 		next unless $rr->typecovered eq 'DNSKEY';
-		$x++ if Valid == sig_is_valid($rr, \@dnskeys, \@dnskeys);
+		$x++ if Valid == sig_is_valid($rr, \@dnskeys, \@ksks);
 	}
 	unless ($x) {
 		problem("Cannot validate DNSKEY RRset with KSKs");